d1ff050535d956b2cf00dd78b136e9181727b8e7
Diogo Alves Morais <diogodih512@gmail.com>
2025-12-01 07:32:39 -0300

    Add font size adjustment script

---
+++ fonte.js
@@ -0,0 +1,35 @@
+<script>
+let tamanhoSalvo = localStorage.getItem("tamanhoFonte");
+
+if (tamanhoSalvo) {
+  document.documentElement.style.fontSize = tamanhoSalvo + "px";
+} else {
+  tamanhoSalvo = 16;
+  document.documentElement.style.fontSize = "16px";
+}
+
+const btnMais = document.getElementById("mais");
+const btnMenos = document.getElementById("menos");
+
+if (btnMais) {
+  btnMais.addEventListener("click", function () {
+    let tamanho = parseInt(localStorage.getItem("tamanhoFonte")) || 16;
+    if (tamanho < 22) {
+      tamanho += 2;
+      document.documentElement.style.fontSize = tamanho + "px";
+      localStorage.setItem("tamanhoFonte", tamanho);
+    }
+  });
+}
+
+if (btnMenos) {
+  btnMenos.addEventListener("click", function () {
+    let tamanho = parseInt(localStorage.getItem("tamanhoFonte")) || 16;
+    if (tamanho > 8) {
+      tamanho -= 2;
+      document.documentElement.style.fontSize = tamanho + "px";
+      localStorage.setItem("tamanhoFonte", tamanho);
+    }
+  });
+}
+</script>
